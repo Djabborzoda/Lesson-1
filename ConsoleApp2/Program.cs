9079d0a7a439485c57039b1c580a88f706c396ec
Djabborzoda <88387626+Djabborzoda@users.noreply.github.com>
2021-08-09 15:28:01 +0500

    Update Program.cs

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,60 +1,49 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
+using System;
 
-namespace ConsoleApp2
+namespace First_Lesson
 {
     class Program
     {
         static void Main(string[] args)
         {
-                                           // Задача №1
-            double a = 16.80;//#1
-            double b = 12.40;
-            Console.WriteLine(Math.Sqrt(a * b));
-            
-                                              //Задача №2
-            double A = 1.40;
-            double B =-5.50;
-            double C = 0.60;
-            double ac = A - C;
-            double bc = C - B;
-            
-            Console.WriteLine("ас " + ac);
-            Console.WriteLine("bc " + bc);
-            Console.WriteLine("ac + ab = " + (ac+bc));
-            
-                                               //Задача №3
-            double x1 = -6.20;
-            double x2 = 2.10;
-            double y1 = 5.20;
-            double y2 = 9.80;
-            Console.WriteLine( Math.Sqrt( Math.Pow(x2-x1,2) + Math.Pow(y2-y1,2)) ); 
-            
-             int ab =41;                       //Задача №4
-             int ba =0;
-             ba =ab/10 +ab%10*10;
-             Console.Write(ba); 
-            
-                                             // Задача №5
-            
-            int N = 10985;      
-            Console.WriteLine (N/60);
-
-                                             // Задача № 6 
-            
-            int Воскресенье = 0;
-            int Понедельник = 1;
-            int Вторник = 2;
-            int Суббота = 6;
+                                   //Task №1 
+            double a = 16.8; 
+            double b = 12.4;
+            double k = Math.Sqrt(a*b);
+            Console.WriteLine(k);
+                                   //Task №2
+            double A = 1.4;
+            double B = -5.5;
+            double  C = 0.6; 
+            double AC = A - C;
+            double BC = C - B;
+            Console.WriteLine(AC);
+            Console.WriteLine(BC);
+            Console.WriteLine(AC+BC);
+                                     //Task №3
+            double x1 = -6.2;
+            double x2 = 2.1;
+            double y1 = 5.2;
+            double y2 = 9.8;
+            double h = Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2));
+            Console.WriteLine(h);
+                                    //Task №4
+            int n = 41;
+            int w = n / 10;
+            int u = n % 10;
+            Console.WriteLine(u+""+w);
+                                   //Task №5
+            int g = 10985;
+            int m = g / 60;
+            Console.WriteLine(m);
+                                  //Task №6
+            int Sunday = 0; 
+            int Monday = 1;
+            int Thuesday = 2;
+            int Saturday = 6;
             int K = 202;
             int result = K % 7;
             Console.WriteLine(result);
-
-
-
-
         }
-
-        
     }
 }
